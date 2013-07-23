f2f286378fa786bd26cef6d9a950bdd0c0b611bf
Felix Nance <fnsport2@aol.com>
2013-07-23 05:07:04 +0000

    Change to port 8080, added support for static files

--- web.js
+++ web.js
@@ -2,12 +2,16 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+app.use('/bootstrap',express.static(__dirname + '/bootstrap'));
+
 app.get('/', function(request, response) {
   var string = new Buffer(fs.readFileSync("index.html"));
   response.send(string.toString());
 });
 
-var port = process.env.PORT || 5000;
+//var port = process.env.PORT || 5000;
+// (Felix) changing port to 8080
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
